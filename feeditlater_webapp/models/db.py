03b6c7c6f75fb5295c16351e2b26f3818ecd0667
Ankur Dahiya <legalos.lotr@gmail.com>
2012-02-23 14:38:08 +0530

    Tested on GAE. works

--- feeditlater_webapp/models/db.py
+++ feeditlater_webapp/models/db.py
@@ -1,4 +1,8 @@
-db = DAL("sqlite://storage.db")
+if request.env.web2py_runtime_gae:
+    db = DAL('gae')
+    session.connect(request, response, db)
+else:
+    db = DAL('sqlite://storage.db')
 
 db.define_table('users',
         Field('email', label="Email", unique=True, notnull=True,
