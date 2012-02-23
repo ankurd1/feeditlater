b2fb06b8dda42f1756f87f41b0f460681380e924
Ankur Dahiya <legalos.lotr@gmail.com>
2012-02-23 12:57:23 +0530

    submit_links working

--- feeditlater_webapp/controllers/default.py
+++ feeditlater_webapp/controllers/default.py
@@ -15,3 +15,17 @@ def index():
             return dict(user_secret=user.secret, user_reg_form=None)
 
     return dict(user_reg_form=user_reg_form, user_secret=None)
+
+def submit_link():
+    if 'secret' in request.get_vars and 'url' in request.get_vars:
+        if (IS_URL()(request.get_vars['url'])[1] is not None):
+            return dict(msg="Invalid")
+        user = db(db.users.secret==request.get_vars['secret']).select().first()
+        if user is None:
+            return dict(msg="Invalid")
+        else:
+            db.links.insert(user_id=user.id, url=request.get_vars['url'])
+            return dict(msg="Success")
+    else:
+        return dict(msg="Invalid")
+
